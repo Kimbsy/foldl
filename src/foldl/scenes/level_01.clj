22682424a720e4ca6da1421aab3eae192d8cf3b8
Kimbsy <lordkimber@gmail.com>
2026-05-15 20:01:05 +0100

    Pretty much working as a POc

--- src/foldl/scenes/level_01.clj
+++ src/foldl/scenes/level_01.clj
@@ -9,6 +9,12 @@
 
 (def coral-pink (p/hex->rgba "#FF9B85"))
 
+(def initial-paper
+  [[200 250]
+   [200 350]
+   [600 350]
+   [600 250]])
+
 (defn sprites
   "The initial list of sprites for this scene"
   [{:keys [window] :as state}]
@@ -18,10 +24,13 @@
   "Make sure the lines intersect using `collision/lines-intersect?`
   before calling this to avoid division by zero."
   [[[x1 y1] [x2 y2]] [[x3 y3] [x4 y4]]]
+
+  ;; @TODO: check if t or u will div-0, only calculate one of them
   (let [t (/ (- (* (- x1 x3) (- y3 y4))
                 (* (- y1 y3) (- x3 x4)))
              (- (* (- x1 x2) (- y3 y4))
-                (* (- y1 y2) (- x3 x4))))]
+                (* (- y1 y2) (- x3 x4))))
+        ]
     [(+ x1 (* t (- x2 x1)))
      (+ y1 (* t (- y2 y1)))]))
 
@@ -76,45 +85,47 @@
         fold-end (mapv + l-start big-ul)]
     [fold-start fold-end]))
 
+;; @TODO: massively reduce what we're drawing every frame
 (defn draw-level-01!
   "Called each frame, draws the current scene to the screen"
   [{:keys [current-scene] :as state}]
   (c/draw-background! coral-pink)
-  (let [{:keys [draw-line? l-start l-end paper]} (get-in state [:scenes current-scene])
+  (let [{:keys [draw-line? l-start l-end papers]} (get-in state [:scenes current-scene])
         l-end (or l-end (i/mouse-pos state))]
-    ;; draw the paper
-    (shape/fill-poly! state [0 0] paper p/white)
-    ;; draw the points
-    (doseq [p paper]
-      (let [size 10
-            offset [(/ size 2) (/ size 2)]]
-        (shape/draw-ellipse! state (mapv - p offset) [size size] p/black)))
-    (when draw-line?
-      ;; draw actual line
-      (shape/draw-line! state l-start l-end p/cyan :line-width 4)
-      ;; calculate fold line
-      (let [[fold-start fold-end :as fold-line] (project-fold-line state l-start l-end)]
-        ;; draw fold line
-        (when (seq (last fold-line))
-          (shape/draw-line! state fold-start l-start p/red)
-          (shape/draw-line! state l-end fold-end p/red)
+    (doseq [[i paper] (map vector (range) papers)]
+      ;; draw the paper
+      (shape/fill-poly! state [0 0] paper (nth (iterate p/darken p/white) i))
+      ;; draw the points
+      (doseq [p paper]
+        (let [size 10
+              offset [(/ size 2) (/ size 2)]]
+          (shape/draw-ellipse! state (mapv - p offset) [size size] p/black)))
+      (when draw-line?
+        ;; draw actual line
+        (shape/draw-line! state l-start l-end p/cyan :line-width 4)
+        ;; calculate fold line
+        (let [[fold-start fold-end :as fold-line] (project-fold-line state l-start l-end)]
+          ;; draw fold line
+          (when (seq (last fold-line))
+            (shape/draw-line! state fold-start l-start p/red)
+            (shape/draw-line! state l-end fold-end p/red)
 
-          ;; tmp show reflections in the fold-line
-          (shape/fill-ellipse! state (i/mouse-pos state) [10 10] p/magenta)
-          (shape/fill-ellipse! state (reflect (i/mouse-pos state)
-                                              fold-line)
-                               [10 10] p/yellow)
+            ;; tmp show reflections in the fold-line
+            (shape/fill-ellipse! state (i/mouse-pos state) [10 10] p/magenta)
+            (shape/fill-ellipse! state (reflect (i/mouse-pos state)
+                                                fold-line)
+                                 [10 10] p/yellow)
 
-          )
-        ;; highlight paper edge intersections
-        (doseq [[a b :as line] (u/poly-lines paper)]
-          (when (collision/lines-intersect? line fold-line)
-            (shape/draw-line! state a b p/green :line-width 3)
-            ;; show intersection points
-            (let [p (line-intersection-point line fold-line)
-                  size 10
-                  offset [(/ size 2) (/ size 2)]]
-              (shape/draw-ellipse! state (mapv - p offset) [size size] p/black))))))))
+            )
+          ;; highlight paper edge intersections
+          (doseq [[a b :as line] (u/poly-lines paper)]
+            (when (collision/lines-intersect? line fold-line)
+              (shape/draw-line! state a b p/green :line-width 3)
+              ;; show intersection points
+              (let [p (line-intersection-point line fold-line)
+                    size 10
+                    offset [(/ size 2) (/ size 2)]]
+                (shape/draw-ellipse! state (mapv - p offset) [size size] p/black)))))))))
 
 (defn update-level-01
   "Called each frame, update the sprites in the current scene"
@@ -123,35 +134,55 @@
       sprite/update-state))
 
 (defn fold-paper
-  [{:keys [current-scene] :as state}]
-  (let [{:keys [l-start l-end paper]} (get-in state [:scenes current-scene])
-        [f-start f-end :as fold-line] (project-fold-line state l-start l-end)
-        ;; @TODO: tween the change, for now snapping to it is fine
-        {:keys [any-intersected? points]}
-        (reduce (fn [{:keys [points] :as acc} [a b :as line]]
+  [paper [f-start f-end :as fold-line]]
+  (let [;; @TODO: tween the change, for now snapping to it is fine
+
+        ;; add new points into poly at intersections
+        {:keys [points i-points]}
+        (reduce (fn [{:keys [points i-points] :as acc} [a b :as line]]
                   (if (collision/lines-intersect? fold-line line)
                     (let [p (line-intersection-point line fold-line)]
                       (-> acc
                           (assoc :points (into points [p b]))
-                          (assoc :any-intersected? true)))
+                          (assoc :i-points (into i-points [p]))))
                     (assoc acc :points (into points [b]))))
                 {:any-intersected? false
-                 :points [(first paper)]}
-                (u/poly-lines paper))
+                 :points [(first paper)]
+                 :i-points []}
+                (u/poly-lines paper))]
+    ;; do nothing if no intersections
+    (if (empty? i-points)
+      {:new-shapes [paper]
+       :i-points []}
+      ;; split into new shapes
+      (let [i-pairs (u/poly-lines i-points)
 
-        new-paper-groups (partition-by (partial u/left-turn? f-start f-end) (butlast points))
+            point-loop (take 100 (cycle points))
 
-        new-paper (if any-intersected?
-                    (mapcat (fn [ps]
-                              (if (u/left-turn? f-start f-end (first ps))
-                                ps
-                                (map #(reflect % fold-line) ps)))
-                            new-paper-groups)
-                    (apply concat new-paper-groups))]
+            new-shapes (map (fn [[i1 i2]]
+                              (let [begin (drop-while #(not= i1 %) (take 15 point-loop))
+                                    shape (take-while #(not= i2 %) begin)
+                                    extra (first (drop (count shape) begin))]
+                                (vec (concat shape [extra]))))
+                            i-pairs)]
+        {:new-shapes new-shapes
+         :i-points i-points}))))
 
-    ;; @TODO: reflecting breaks triangulation! we need to add a new shape, not just move points. take a look at folding an ear of the paper.
-
-    (assoc-in state [:scenes current-scene :paper] (vec new-paper))))
+(defn fold-all-papers
+  [{:keys [current-scene] :as state}]
+  (let [{:keys [l-start l-end papers]} (get-in state [:scenes current-scene])
+        [f-start f-end :as fold-line] (project-fold-line state l-start l-end)
+        fold-results (map #(fold-paper % fold-line) papers)
+        new-papers (mapcat :new-shapes fold-results)
+        i-points (into #{} (mapcat :i-points fold-results))
+        reflected (if (seq i-points)
+                    (map (fn [paper]
+                           (if (some #(u/left-turn? f-start f-end %) (remove i-points paper))
+                             (vec (reverse (map #(reflect % fold-line) paper)))
+                             paper))
+                         new-papers)
+                    new-papers)]
+    (assoc-in state [:scenes current-scene :papers] (vec reflected))))
 
 (defn handle-click
   [{:keys [current-scene] :as state} e]
@@ -169,12 +200,18 @@
              (not= l-start (:pos e)))
         (-> state
             (assoc-in [:scenes current-scene :l-end] (:pos e))
-            fold-paper)
+            fold-all-papers)
 
         :else
         (-> state
             (assoc-in [:scenes current-scene :draw-line?] true)
             (assoc-in [:scenes current-scene :l-start] (:pos e)))))
+    state))
+
+(defn handle-key
+  [{:keys [current-scene] :as state} e]
+  (if (i/is e :key i/K_ESCAPE)
+    (assoc-in state [:scenes current-scene :papers] [initial-paper])
     state))
 
 (defn init
@@ -184,10 +221,9 @@
    :draw-fn draw-level-01!
    :update-fn update-level-01
    :mouse-button-fns [handle-click]
+   :key-fns [handle-key]
    :draw-line? false
    :l-start nil
    :l-end nil
-   :paper [[200 250]
-           [200 350]
-           [600 350]
-           [600 250]]})
+   :papers [;; start with 1
+            initial-paper]})
